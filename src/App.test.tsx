a073d23f137ab4c89d34bacc4170bd305da60f62
jayce <phat.thai@mobiclixgroup.com>
2019-12-10 11:20:56 +0700

    add unit test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,21 @@
-import React from "react";
-import { render } from "@testing-library/react";
-import App from "./routes/App";
+import { EmailRegex, PhoneRegex } from "./shared/utils";
 
-test("renders learn react link", () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test("test string is Email", () => {
+  // expect(null).toMatch(EmailRegex);
+  // expect(undefined).toMatch(EmailRegex);
+  // expect("").toMatch(EmailRegex);
+  // expect("afadsfdas").toMatch(EmailRegex);
+  // expect(123123).toMatch(EmailRegex);
+  // expect(32121321321.32).toMatch(EmailRegex);
+  expect("fdsafds@gmail.com").toMatch(EmailRegex);
+});
+
+test("test string is phone", () => {
+  // expect(null).toMatch(PhoneRegex);
+  // expect(undefined).toMatch(PhoneRegex);
+  // expect(false).toMatch(PhoneRegex);
+  // expect("1345678").toMatch(PhoneRegex);
+  // expect("").toMatch(PhoneRegex);
+  // expect("0.23").toMatch(PhoneRegex);
+  expect("0902751467").toMatch(PhoneRegex);
 });
